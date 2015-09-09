49939ab33d97e49c0bbd74995937d915e27cada4
Dave Pfister <davepfister@comcast.net>
2015-09-08 21:06:37 -0400

    copy students to get it working

--- project2/js/megaroster.js
+++ project2/js/megaroster.js
@@ -1,10 +1,42 @@
 var Megaroster = function() {
   var self = this;
-};
 
-this.init = function(){
-  console.log('roster init');
+  this.save = function() {
+    try {
+        return (localStorage.students = JSON.stringify(self.students));
+    }
+    catch(err) {
+      return false;
+    }
+  };
+
+  this.addStudent = function(student_name) {
+    var student = new Student();
+    student.init({
+      name: student_name
+    });
+
+    self.students.push(student);
+    student.appendToList();
+
+    self.save();
+  };
+
+  this.init = function() {
+    self.students = [];
+    Student.counter = 0;
+    //self.load();
+    //Student.counter = 0;
+
+      $(document).on('submit', 'form.edit', self.updateStudent);
+
+      $('#new_student_form').on('submit', function(ev){
+        ev.preventDefault();
+        var student_name = $(this.student_name).val();
+        self.addStudent(student_name);
+      });
+  };
 };
 
 var roster = new Megaroster();
-// roster.init();
+roster.init();
---
+++ project2/js/student.js
@@ -0,0 +1,45 @@
+var Student = function() {
+  var self = this;
+
+  self.getOrSetId = function(id) {
+    if (!id) {
+      id = Student.counter + 1;
+      Student.counter ++; // TODO: We must fix this everntually.
+    }
+    self.incrementCounter(id);
+    return id;
+  };
+  self.incrementCounter = function(id) {
+    if(id > Student.counter) {
+      Student.counter = id;
+    }
+  };
+
+  self.init = function(properties) {
+    self.name = properties.name;
+
+    self.id = self.getOrSetId(properties.id);
+  };
+
+  self.appendToList = function(){
+    var li = $('#list_item_template').clone();
+    li.removeAttr('id')
+    .attr('data-id', self.id)
+      .addClass('student')
+
+      .removeClass('hidden');
+    li.find('label').append(self.name);
+    $('#students').append(li)
+  }
+};
+
+Student.getStudentById = function(id) {
+  var student;
+  $.each(roster.students, function(index, current_student) {
+    if (current_student.id.toString() === id.toString()) {
+      student = current_student;
+      return false;
+    }
+  });
+  return student;
+};
